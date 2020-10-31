f5e7644a6245aef33e0416998231d2d28c2bdffa
ssomma <sebastian.somma1@gmail.com>
2020-10-30 23:54:17 -0300

    First Functional Commit;

---
+++ src/main/java/com/dna/tester/mutantvalidator/configuration/RedisConfiguration.java
@@ -0,0 +1,29 @@
+package com.dna.tester.mutantvalidator.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.math.BigInteger;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("127.0.0.1", 6379);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, BigInteger> redisTemplate() {
+        RedisTemplate<String, BigInteger> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
+
+}
